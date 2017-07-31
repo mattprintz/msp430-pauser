0d1bfe750a0ba322467c2bb21ea582c6d24162c3
matt.printz <matt.printz@undercovertourist.com>
2017-07-31 12:15:36 -0500

    Uses dbus to pause services

--- linux/pauseService.py
+++ linux/pauseService.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 
 import dbus
-import dbus.service
-from dbus.mainloop.glib import DBusGMainLoop
 import gtk
 import time
 import thread
@@ -10,32 +8,55 @@ import threading
 import serial
 import sys
 
+
 class Watcher(threading.Thread):
-    
-    def __init__(self, dev, pauser):
+
+    def __init__(self, dev):
         threading.Thread.__init__(self)
         self.bus = dbus.SessionBus()
-        
+
         self.dev = dev
-        self.pauser = pauser
         self.killed = False
-    
+
+    def getMediaServices(self):
+        service_names = [str(name) for name in self.bus.list_names() if str(name).startswith('org.mpris.MediaPlayer2')]
+        proxies = [self.bus.get_object(name, '/org/mpris/MediaPlayer2') for name in service_names]
+        ifaces = [dbus.Interface(proxy, dbus_interface="org.mpris.MediaPlayer2.Player") for proxy in proxies]
+        return ifaces
+
+    def pause(self):
+        ifaces = self.getMediaServices()
+        for iface in ifaces:
+            iface.Pause()
+
+    def play(self):
+        ifaces = self.getMediaServices()
+        for iface in ifaces:
+            iface.Pause()
+            iface.PlayPause()
+
+    def playPause(self):
+        ifaces = self.getMediaServices()
+        for iface in ifaces:
+            iface.PlayPause()
+
     def run(self):
         try:
-            
+
             self.dev.flushInput()
             while not self.killed:
                 char = self.dev.read(1)
+                self.dev.flushInput()
                 if char:
                     if char == 'U':
                         print "Click up"
-                        self.pauser.clickUp()
+                        self.play()
+                        time.sleep(0.1)
                     elif char == 'D':
                         print "Click down"
-                        self.pauser.clickDown()
-                    
-                self.dev.flushInput()
-                
+                        self.pause()
+                        time.sleep(0.1)
+
             print "Shutting down..."
             self.dev.write("Z") # Send reset signal to device
             self.dev.close()
@@ -43,67 +64,62 @@ class Watcher(threading.Thread):
         except:
             print "Shutting down..."
             gtk.main_quit()
-    
+
     def kill(self):
         self.killed = True
 
 
 class SerialInit:
-    def __init__(self, pauser):
-        
+    def __init__(self):
+
         self.running = False
-        
         term = "/dev/pauser"
-        
         dev = None
-        
+
         while not self.running:
             try:
                 print "Trying ", term
                 dev = serial.Serial(port=term, baudrate=9600, timeout=1)
-                
+
                 assert dev
-                
+
                 self.running = self.initDevice(dev)
-                
+
                 if self.running:
                     break
-                
-                
+
                 # No devices found
                 assert dev
             except:
                 print "Can't open device"
                 time.sleep(5)
                 continue
-            
-        
+
         print "Ready"
-        
-        self.background = Watcher(dev, pauser)
+
+        self.background = Watcher(dev)
         self.background.start()
-    
-    
+
     def thread(self):
         return self.background.currentThread()
-    
-    
+
     def initDevice(self, dev):
         result = False
-        
+
         dev.flushInput();
         dev.flushOutput();
-        
+
         print "Sending init"
         dev.write("!")
-        
+
         char = dev.read(1)
-        
+
         if char:
+            print char
             if char == "00".decode("hex"):
                 print "Found Null"
                 char = dev.read(1)
-            
+
             if char == "#":
                 print char
                 dev.write("#")
@@ -123,43 +139,12 @@ class SerialInit:
                 print "Unexpected char: " + char
         else:
             print "No Char!"
-            
-        
-        
-        
+
         return result
-    
-
-class Pauser(dbus.service.Object):
-    def __init__(self):
-        busName = dbus.service.BusName('org.hipokrit.pauser', bus = dbus.SessionBus())
-        dbus.service.Object.__init__(self, busName, '/Pauser')
-    
-    @dbus.service.method('org.hipokrit.pauser', in_signature = '', out_signature = 'b')
-    def click(self):
-        self.clicked()
-        return True
-    
-    @dbus.service.method('org.hipokrit.pauser', in_signature = '', out_signature = 'b')
-    def clickUp(self):
-        self.clicked(True)
-        return True
-    
-    @dbus.service.method('org.hipokrit.pauser', in_signature = '', out_signature = 'b')
-    def clickDown(self):
-        self.clicked(False)
-        return True
-    
-    @dbus.service.signal(dbus_interface='org.hipokrit.pauser', signature='b')
-    def clicked(self, up):
-        return up
-
 
 
 gtk.gdk.threads_init()
-myMainLoop = DBusGMainLoop(set_as_default = True)
-myservice = Pauser()
-serialInput = SerialInit(myservice)
+serialInput = SerialInit()
 try:
     gtk.main()
 finally:
